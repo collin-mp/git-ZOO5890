88e578511cef04b17811607042755efe3f251b3c
collin-mp <cporte26@uwyo.edu>
2026-09-14 11:40:42 -0400

    Test commit for new Git repo

---
+++ git-test.py
@@ -0,0 +1,20 @@
+# small script for practicing Git commands
+
+# print a simple greeting so the first commit has visible output
+print("Hello from my Git practice file!")
+
+# Store the ASCII-art version of the word "Hello!" in a multi-line string
+hello_art = r"""
+ #   #  ######  #       #        ###  
+ #   #  #       #       #       #   # 
+ #####  #####   #       #       #     
+ #   #  #       #       #       #   # 
+ #   #  ######  ######  ######   ###  
+                                      
+"""
+
+# print a heading before showing the ASCII art
+print("\nASCII art:")
+
+# print the completed ASCII-art greeting
+print(hello_art)
